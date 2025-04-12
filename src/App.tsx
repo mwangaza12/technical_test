a107a6cb13c55185e205e2933be229a623337ba4
mwangaza12 <mwangaza12>
2025-04-12 10:50:29 +0300

    added section 1 contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './index.css'
+import { Sparkle, MousePointer2 } from 'lucide-react'
+
 
 function App() {
 
@@ -6,9 +8,16 @@ function App() {
     <div className="flex h-screen w-full justify-center items-center bg-gray-50">
       <div className="grid grid-cols-10 grid-rows-10 gap-6 h-full w-full px-12 py-8 m-16">
 
-        {/* Section 1: Create and schedule content */}
-        <div className="col-span-2 row-span-5 bg-amber-50 rounded-xl p-6 relative">
-          
+       {/* Section 1: Create and schedule content */}
+       <div className="col-span-2 row-span-5 bg-amber-50 rounded-xl p-6 relative">
+          <h2 className="text-2xl font-normal mb-3 tracking-tighter leading-6">Create and schedule <br /> content <br /><span className="italic text-indigo-600">quicker.</span></h2>
+          <button className="bg-amber-400 px-5 py-2 rounded-full mt-6 font-medium flex items-center shadow-md hover:bg-amber-500 transition-colors text-purple-600">
+            Create Post
+            <Sparkle className='size-4 pl-1 fill-purple-600'/>
+          </button>
+          <div className="absolute bottom-23 right-28">
+            <MousePointer2 className='absolute rotate-80 fill-black'/>
+          </div>
         </div>
 
         {/* Section 2: Hero banner */}
